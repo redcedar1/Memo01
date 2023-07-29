3182944bbb78782eb64b4c5b8cc08b59c721b5a6
ki <redcedar1@naver.com>
2023-07-29 13:32:38 +0900

    init commit

---
+++ src/test/java/com/sparta/memo07/Memo07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memo07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memo07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
