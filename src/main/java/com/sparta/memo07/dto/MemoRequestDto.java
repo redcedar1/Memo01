98d8392522ede28ca94fc32009e3ac6f13eded22
ki <redcedar1@naver.com>
2023-07-29 15:06:09 +0900

    Create, Read 구현

---
+++ src/main/java/com/sparta/memo07/dto/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.memo07.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
---
+++ src/main/java/com/sparta/memo07/dto/MemoResponseDto.java
@@ -0,0 +1,17 @@
+package com.sparta.memo07.dto;
+
+import com.sparta.memo07.entity.Memo;
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
+}
